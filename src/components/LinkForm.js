7ea1f1a14f059220b2469d156ddc60320c615901
NicolasDeHorta <dehorta.n@gmail.com>
2021-11-02 17:17:30 -0300

    replaced alert with swal, its prettier

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from "react";
 import { db } from "../firebase";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 
 export const LinkForm = (props) => {
@@ -16,13 +18,23 @@ export const LinkForm = (props) => {
         return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(str);
       }
 
+      const swalert = async (typeOfChange, icon) => {
+        const MySwal = withReactContent(Swal)
+ 
+        await MySwal.fire({
+          title: <strong>{typeOfChange}</strong>,
+          html: <i>remember to add https://</i>,
+          icon: icon
+        })
+    }
+
     const handleSubmit = e => {
         e.preventDefault()
         if (validateUrl(linkData.url)) {
             props.addOrEditLink(linkData)
             setLinkData({...initialLinkData})
         } else {
-            alert("url invalido")
+            swalert("invalid URL", "error")
         }
         
     }
